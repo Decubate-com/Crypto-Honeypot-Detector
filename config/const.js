325dea96ee0d6eb4f5fc46f19a0022ecb810b6f9
Aaron Fitzpatrick <a.fitzy98@gmail.com>
2022-08-30 18:27:06 +0200

    added constants for BSC & using websocket

--- config/const.js
+++ config/const.js
@@ -9,20 +9,23 @@ export const maxBuyFee = 10;
 export const maxSellFee = 10;
 
 // Owner address
-export const ownerAddress = '';
+export const ownerAddress = "0xfFd9C0995515AB00135ae30bd02c432F9f91c673";
+
+// Ws Providers
+export const BSCwsprovider = "ws://168.119.6.59:8545";
 
 // Http providers
-export const BSChttpprovider = 'https://bsc-dataseed.binance.org/';
-export const ETHERhttpprovider = 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
-export const AVAXhttpprovider = 'https://api.avax.network/ext/bc/C/rpc';
-export const FTMhttpprovider = 'https://rpc.ftm.tools';
-export const MATIChttpprovider = 'https://polygon-rpc.com';
-export const XDAIhttpprovider = 'https://rpc.gnosischain.com/';
+export const ETHERhttpprovider =
+  "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+export const AVAXhttpprovider = "https://api.avax.network/ext/bc/C/rpc";
+export const FTMhttpprovider = "https://rpc.ftm.tools";
+export const MATIChttpprovider = "https://polygon-rpc.com";
+export const XDAIhttpprovider = "https://rpc.gnosischain.com/";
 
 // Multicall addresses
-export const BSCaddress = '';
-export const ETHERaddress = '';
-export const AVAXaddress = '';
-export const FTMaddress = '';
-export const MATICaddress = '';
-export const XDAIaddress = '';
+export const BSCaddress = "0x57De61536DC728d33eE531fa41ce62067B718aB6";
+export const ETHERaddress = "";
+export const AVAXaddress = "";
+export const FTMaddress = "";
+export const MATICaddress = "";
+export const XDAIaddress = "";
